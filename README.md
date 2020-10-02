ca1e41e0be8f47269d5b1f646c79992af0be7236
ThribhuvanGuptaS <thribhuvangupta@gmail.com>
2020-10-02 19:07:11 +0530

    duniya badalne se rok dia

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
-# maister
-A healthcare AI suite 
-
 # ClinicSync
-
-## AGBI Digital HealthTech Grand Challenge
-
-https://www.hackerearth.com/challenges/hackathon/agbi-digital-healthtech-grand-challenge/
-
-## Model link
-
-https://drive.google.com/drive/folders/1j6XiDT5sx1nXa_jINgpBxOOIi_TridQM?usp=sharing
+A healthcare AI suite 
--- runserver.py
+++ runserver.py
@@ -1,14 +1,14 @@
 """
-This script runs the Maister application using a development server.
+This script runs the ClinicSync application using a development server.
 """
 
 from os import environ,system
-from maister import app
+from clinicsync import app
 from gevent.pywsgi import WSGIServer
 
 if __name__ == '__main__':
     # app.run(port=5002, threaded=False)
 
     # Serve the app with gevent
-    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server = WSGIServer(('0.0.0.0', 5002), app)
     http_server.serve_forever()
