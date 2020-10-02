03a8771b8da498fc43377615c725d0f589617b9f
Rajesh Hegde <rajeshhegde180@gmail.com>
2020-10-02 20:54:54 +0530

    duniya badal diya returns

--- clinicsync/views.py
+++ clinicsync/views.py
@@ -1,8 +1,9 @@
 from flask import url_for, request, render_template, Response, jsonify, redirect
-from clinicsync import app,model_predict,model
+from clinicsync import app, model_predict, model
 import numpy as np
 from clinicsync.util import base64_to_pil
 from datetime import datetime
+
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
@@ -24,7 +25,7 @@ def predict():
             pred_class = "PNUEMONIA"
         # Process your result for human
         pred_proba = "{:.3f}".format(np.amax(preds))    # Max probability
-        #pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
+        # pred_class = decode_predictions(preds, top=1)   # ImageNet Decode
 
         result = str(pred_class)               # Convert to string
         result = result.replace('_', ' ').capitalize()
@@ -38,18 +39,16 @@ def predict():
 def pneumonia():
     return render_template('pneumonia.html', title='Pneumonia', year=datetime.now().year)
 
+
 @app.route('/scanner')
 def scanner():
     return render_template('scanner.html', title='Scanner', year=datetime.now().year)
 
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html',title='Home',year=datetime.now().year)
+    return render_template('home.html', title='Home', year=datetime.now().year)
 
-@app.route('/about')
-def about():
-    """Renders the about page."""
-    return render_template('about.html',title='About',year=datetime.now().year,message='Your application description page.')
 
 # Add backend stuff for new diseases from here :
